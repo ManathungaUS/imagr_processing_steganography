623fc3fd3bff75349a8f94f3bcb5abf4ebe3149b
ManathungaUS <usmredhood@gmail.com>
2022-03-01 10:46:20 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,26 @@
+var imagedatauri;
+
+function readURL(input) {
+  var reader = new FileReader();
+  reader.onload = function (e) {
+    imagedatauri = e.target.result;
+    document.querySelector('#image1').src = e.target.result;
+  };
+  reader.readAsDataURL(input.files[0]);
+}
+
+function hideText() {
+  document.querySelector('#image2').src = steg.encode(
+    document.querySelector('#text').value,
+    imagedatauri
+  );
+}
+
+function decode(input) {
+  var reader = new FileReader();
+
+  reader.onload = function (e) {
+    document.querySelector('#decoded').innerHTML = steg.decode(e.target.result);
+  };
+  reader.readAsDataURL(input.files[0]);
+}
